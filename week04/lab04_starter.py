df0c71c06ef6ae2c48ec52fece7df1d41b71cd21
Your Name <you@example.com>
2025-01-31 09:02:41 -0500

    added belt

--- week04/lab04_starter.py
+++ week04/lab04_starter.py
@@ -81,6 +81,9 @@ loot_options = ["Health Potion", "Poison Potion", "Secret Note", "Leather Boots"
 good_loot_options = ["Health Potion", "Leather Boots"]
 bad_loot_options = ["Poison Potion"]
 
+# belt smth
+belt = []
+
 monster_power = {
     "Fire Magic": 2,
     "Freezing Time": 4,
