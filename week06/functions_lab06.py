0305aff55ec2264a519092860dfeb3b9608b673f
KamiDummy <pin.inci@hotmail.com>
2025-02-14 12:51:24 -0500

    finished

--- week06/functions_lab06.py
+++ week06/functions_lab06.py
@@ -1,15 +1,12 @@
-# Import the random library to use for the dice later
 import random
+
 
 # Will the line below print when you import function.py into main.py?
 # print("Inside function.py")
-
-
 # Lab 4: Question 4
 def use_loot(belt, health_points):
     good_loot_options = ["Health Potion", "Leather Boots"]
     bad_loot_options = ["Poison Potion"]
-
     print("    |    !!You see a monster in the distance! So you quickly use your first item:")
     first_item = belt.pop(0)
     if first_item in good_loot_options:
@@ -26,20 +23,20 @@ def use_loot(belt, health_points):
 # Lab 4: Question 3
 def collect_loot(loot_options, belt):
     ascii_image3 = """
-                      @@@ @@                
-             *# ,        @              
-           @           @                
-                @@@@@@@@                
-               @   @ @% @*              
-            @     @   ,    &@           
-          @                   @         
-         @                     @        
-        @                       @       
-        @                       @       
-        @*                     @        
-          @                  @@         
-              @@@@@@@@@@@@          
-              """
+                            @@@ @@                
+                   *# ,        @              
+                 @           @                
+                      @@@@@@@@                
+                     @   @ @% @*              
+                  @     @   ,    &@           
+                @                   @         
+               @                     @        
+              @                       @       
+              @                       @       
+              @*                     @        
+                @                  @@         
+                    @@@@@@@@@@@@          
+                    """
     print(ascii_image3)
     loot_roll = random.choice(range(1, len(loot_options) + 1))
     loot = loot_options.pop(loot_roll - 1)
@@ -51,25 +48,24 @@ def collect_loot(loot_options, belt):
 # Hero's Attack Function
 def hero_attacks(combat_strength, m_health_points):
     ascii_image = """
-                                @@   @@ 
-                                @    @  
-                                @   @   
-               @@@@@@          @@  @    
-            @@       @@        @ @@     
-           @%         @     @@@ @       
-            @        @@     @@@@@     
-               @@@@@        @@       
-               @    @@@@                
-          @@@ @@                        
-       @@     @                         
-   @@*       @                          
-   @        @@                          
-           @@                                                    
-         @   @@@@@@@                    
-        @            @                  
-      @              @                  
-
-  """
+                                      @@   @@ 
+                                      @    @  
+                                      @   @   
+                     @@@@@@          @@  @    
+                  @@       @@        @ @@     
+                 @%         @     @@@ @       
+                  @        @@     @@@@@     
+                     @@@@@        @@       
+                     @    @@@@                
+                @@@ @@                        
+             @@     @                         
+         @@*       @                          
+         @        @@                          
+                 @@                                                    
+               @   @@@@@@@                    
+              @            @                  
+            @              @                  
+        """
     print(ascii_image)
     print("    |    Player's weapon (" + str(combat_strength) + ") ---> Monster (" + str(m_health_points) + ")")
     if combat_strength >= m_health_points:
@@ -79,7 +75,6 @@ def hero_attacks(combat_strength, m_health_points):
     else:
         # Player only damaged the monster
         m_health_points -= combat_strength
-
         print("    |    You have reduced the monster's health to: " + str(m_health_points))
     return m_health_points
 
@@ -87,21 +82,21 @@ def hero_attacks(combat_strength, m_health_points):
 # Monster's Attack Function
 def monster_attacks(m_combat_strength, health_points):
     ascii_image2 = """                                                                 
-           @@@@ @                           
-      (     @*&@  ,                         
-    @               %                       
-     &#(@(@%@@@@@*   /                      
-      @@@@@.                                
-               @       /                    
-                %         @                 
-            ,(@(*/           %              
-               @ (  .@#                 @   
-                          @           .@@. @
-                   @         ,              
-                      @       @ .@          
-                             @              
-                          *(*  *      
-             """
+                 @@@@ @                           
+            (     @*&@  ,                         
+          @               %                       
+           &#(@(@%@@@@@*   /                      
+            @@@@@.                                
+                     @       /                    
+                      %         @                 
+                  ,(@(*/           %              
+                     @ (  .@#                 @   
+                                @           .@@. @
+                         @         ,              
+                            @       @ .@          
+                                   @              
+                                *(*  *      
+                   """
     print(ascii_image2)
     print("    |    Monster's Claw (" + str(m_combat_strength) + ") ---> Player (" + str(health_points) + ")")
     if m_combat_strength >= health_points:
@@ -113,6 +108,7 @@ def monster_attacks(m_combat_strength, health_points):
         health_points -= m_combat_strength
         print("    |    The monster has reduced Player's health to: " + str(health_points))
     return health_points
+
 
 # Lab 5: Question 7
 # Recursion
@@ -126,7 +122,6 @@ def inception_dream(num_dream_lvls):
         input("Start to go back to real life? (Press Enter)")
         print("    |    You start to regress back through your dreams to real life.")
         return 2
-
     # Recursive Case
     else:
         # inception_dream(5)
@@ -137,8 +132,43 @@ def inception_dream(num_dream_lvls):
         # 1 + 1 + 1 + 1 + 2
         return 1 + int(inception_dream(num_dream_lvls - 1))
 
-# Lab 06 - Question 3 and 4
 
-# Lab 06 - Question 5a
+# Lab 06 - Question 3 and 4
+def save_game(winner, hero_name="", num_stars=0):
+    with open("save.txt", "a") as file:
+        if winner == "Hero":
+            file.write(f"Hero {hero_name} has killed the monster and gained {num_stars} stars.\n")
+        elif winner == "Monster":
+            file.write(f"Monster killed the {hero_name}.\n")
+        file.close()
+        # Lab 06 - Question 5a
+
+
+def load_game():
+    try:
+        with open("save.txt", "r") as file:
+            print("   |   Loading from saved file..")
+            lines = file.readlines()
+            if lines:
+                last_line = lines[-1].strip()
+                print(last_line)
+                return last_line
+    except FileNotFoundError:
+        print("    |    No previous game found. Starting fresh..")
+        return None
+
 
 # Lab 06 - Question 5b
+def adjust_combat_strength(combat_strength, m_combat_strength):
+    last_game = load_game()
+    if last_game:
+        if "Hero" in last_game and "gained" in last_game:
+            num_stars = int(last_game.split()[-2])
+            if num_stars >= 3:
+                print("    |    Increasing the Monster combat strength!")
+                m_combat_strength += 1
+        elif "Monster killed the" in last_game:
+            print("    |    Increasing the Monster combat strength!")
+            combat_strength += 1
+        else:
+            print("    |    last game had no effect on Hero/Monster combat strength!")
